c6782727e7b37a01c164964392dc047af0c8b711
Antony Reshetov <antonyreshetov@gmail.com>
2020-01-29 16:43:22 +0300

    Update main.js

---
+++ 1.1-variables/main.js
@@ -0,0 +1,23 @@
+//task1
+
+let algebra = 3;
+let geography = 5;
+let physics = 2;
+
+let averageMark = (algebra + geography + physics)/3;
+console.log(averageMark);
+
+//task2
+
+let myName = "Антон";
+let message = `Привет, мир! Меня зовут ${myName}.`;
+console.log(message);
+
+//task3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
